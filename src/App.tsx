3c0a35effa55b789a038f3b252331428ac9e6788
tajava2006 <tajava2006@gmail.com>
2025-04-04 18:45:52 +0900

    chg: tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { bytesToHex } from '@noble/hashes/utils' // already an installed dependency
+import { generateSecretKey, getPublicKey } from 'nostr-tools/pure';
+
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pubkey, setPubkey] = useState<string | null>(null);
+
+  useEffect(() => {
+    const priv = generateSecretKey();
+    const privHex = bytesToHex(priv);
+    console.log(privHex, 2534);
+    const pub = getPublicKey(priv);
+    setPubkey(pub);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>nostr-tools + Bun + React</h1>
+      <p>👤 Generated pubkey:</p>
+      <code>{pubkey}</code>
     </div>
   );
 }
 
-export default App;
+export default App; 
